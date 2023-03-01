2f37a83fec1c26a2c636eaf4a2bda24011a2d995
tlegoc <theo@legoc.net>
2023-03-01 13:15:54 +0100

    Added sound play script

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -7,14 +7,20 @@ public class Script : MonoBehaviour
     public float speed = 5;
     private bool isColliding = false;
 
+    public AudioSource audioSource;
+    public AudioClip jumpSound;
+
     // Update is called once per frame
     void Update()
     {
         Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.position += new Vector3(movement.x, movement.y, 0) * Time.deltaTime * speed;
 
-        if (Input.GetButtonDown("Jump") && isColliding)
+        if (Input.GetButtonDown("Jump") && isColliding) {
             Debug.Log("Oui");
+
+            audioSource.PlayOneShot(jumpSound);
+        }
     }
 
     // Peut poser des problèmes quand on entre/sort dans plusieurs objets en même temps
